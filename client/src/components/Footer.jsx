c47e9ac0c3214a60679a958bc096f9adafd6f617
AramKim37 <aramkim37@gmail.com>
2024-03-15 21:24:29 -0600

    add footer border on the top

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import { Footer } from "flowbite-react";
 const FooterComponent = () => {
-  return <Footer></Footer>;
+  return (
+    <Footer container className="border border-t-4 border-blue-400">
+      Copyright by Aram Kim
+    </Footer>
+  );
 };
 
 export default FooterComponent;
