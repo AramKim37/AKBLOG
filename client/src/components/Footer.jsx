d0b03e5ec1af858f30537b8b5adbc0217f3587e3
AramKim37 <aramkim37@gmail.com>
2024-03-16 22:05:50 -0600

    add css in footer and make auth file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import { Footer } from "flowbite-react";
 const FooterComponent = () => {
   return (
-    <Footer container className="border border-t-4 border-blue-400">
-      Copyright by Aram Kim
+    <Footer
+      container
+      className="w-full border border-t-4 border-blue-400 flex items-center justify-center"
+    >
+      <h1 className="text-center flex items-center justify-center w-full">
+        Aram`blog
+      </h1>
     </Footer>
   );
 };
